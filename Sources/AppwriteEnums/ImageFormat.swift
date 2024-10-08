3c5b39188a478305fb6822d12105dbca3178708d
loks0n <22452787+loks0n@users.noreply.github.com>
2024-10-08 16:17:05 +0100

    feat: multipart

--- Sources/AppwriteEnums/ImageFormat.swift
+++ Sources/AppwriteEnums/ImageFormat.swift
@@ -6,6 +6,7 @@ public enum ImageFormat: String, Codable {
     case gif = "gif"
     case png = "png"
     case webp = "webp"
+    case avif = "avif"
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.singleValueContainer()
