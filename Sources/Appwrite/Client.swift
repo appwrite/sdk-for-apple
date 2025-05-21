a19ee5d2c518672685e147a123790f62dc6530fb
root <chiragaggarwal5k@gmail.com>
2025-05-21 06:27:17 +0000

    chore: bump to minor version

--- Sources/Appwrite/Client.swift
+++ Sources/Appwrite/Client.swift
@@ -23,7 +23,7 @@ open class Client {
         "x-sdk-name": "Apple",
         "x-sdk-platform": "client",
         "x-sdk-language": "apple",
-        "x-sdk-version": "10.0.1",
+        "x-sdk-version": "10.1.0",
         "x-appwrite-response-format": "1.7.0"
     ]
 
