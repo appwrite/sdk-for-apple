d61ce34e246c1ec115f67794a61790b2faa98f15
Jake Barnby <jakeb994@gmail.com>
2025-10-09 20:20:50 +1300

    Add transactions

---
+++ Sources/AppwriteModels/TransactionList.swift
@@ -0,0 +1,54 @@
+import Foundation
+import JSONCodable
+
+/// Transaction List
+open class TransactionList: Codable {
+
+    enum CodingKeys: String, CodingKey {
+        case total = "total"
+        case transactions = "transactions"
+    }
+
+    /// Total number of transactions that matched your query.
+    public let total: Int
+
+    /// List of transactions.
+    public let transactions: [Transaction]
+
+
+    init(
+        total: Int,
+        transactions: [Transaction]
+    ) {
+        self.total = total
+        self.transactions = transactions
+    }
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.total = try container.decode(Int.self, forKey: .total)
+        self.transactions = try container.decode([Transaction].self, forKey: .transactions)
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        try container.encode(total, forKey: .total)
+        try container.encode(transactions, forKey: .transactions)
+    }
+
+    public func toMap() -> [String: Any] {
+        return [
+            "total": total as Any,
+            "transactions": transactions.map { $0.toMap() } as Any
+        ]
+    }
+
+    public static func from(map: [String: Any] ) -> TransactionList {
+        return TransactionList(
+            total: map["total"] as! Int,
+            transactions: (map["transactions"] as! [[String: Any]]).map { Transaction.from(map: $0) }
+        )
+    }
+}
--- docs/examples/tablesdb/get-transaction.md
+++ docs/examples/tablesdb/get-transaction.md
@@ -6,10 +6,7 @@ let client = Client()
 
 let tablesDB = TablesDB(client)
 
-let result = try await tablesDB.deleteRow(
-    databaseId: "<DATABASE_ID>",
-    tableId: "<TABLE_ID>",
-    rowId: "<ROW_ID>",
-    transactionId: "<TRANSACTION_ID>" // optional
+let transaction = try await tablesDB.getTransaction(
+    transactionId: "<TRANSACTION_ID>"
 )
 
